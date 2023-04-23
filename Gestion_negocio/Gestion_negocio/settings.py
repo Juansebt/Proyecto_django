1f97962474c1ab7949499a65f3916899d4d697bb
Juan Sebastian Laguna <juansebt.0610@gmail.com>
2023-04-23 11:59:21 -0500

    update

--- Gestion_negocio/Gestion_negocio/settings.py
+++ Gestion_negocio/Gestion_negocio/settings.py
@@ -75,15 +75,14 @@ WSGI_APPLICATION = 'Gestion_negocio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -94,6 +93,7 @@ DATABASES = {
         'PORT': '3306'
     }
 }
+"""
 
 
 # Password validation
